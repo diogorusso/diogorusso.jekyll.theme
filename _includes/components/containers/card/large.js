f095eb05cf486822f90edd7a2487bdced329319a
Diogo Russo Ribeiro <diogo.russo.ribeiro@climate.com>
2019-03-06 16:44:45 -0800

    slick

--- _includes/components/containers/card/large.js
+++ _includes/components/containers/card/large.js
@@ -1,20 +1,20 @@
-$(function () {
+jQuery(document).ready(function($) {
     
-    var $arrows = $('.arrows');
-    var $next = $arrows.children(".slick-next");    
-    var $prev = $arrows.children(".slick-prev");
-    
-    var slick = $('.your-class').slick({
-        appendArrows: $arrows
-    });
 
-    $('.slick-next').on('click', function (e) {
-        var i = $next.index( this )
-        slick.eq(i).slickNext();
-    });
-    $('.slick-prev').on('click', function (e) {
-        var i = $prev.index( this )
-        slick.eq(i).slickPrev();
+    $('.your-class').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: false,
+        lazyLoad: 'ondemand',
+        // infinite: false,
+        // slidesToShow: 1,
+        // slidesToScroll: 1,
+        // arrows: true,
+        dots:true,
+        // fade: true,
+        // asNavFor: '.process-pager-slider'
+        adaptiveHeight: false,
     });
 
 });
