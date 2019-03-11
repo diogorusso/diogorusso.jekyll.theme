4b401859d8666f947e29e07dc59d6744c71baa80
diogorusso <hello.diogorusso@gmail.com>
2019-03-10 23:10:22 -0700

    theme

--- gulpfile.js
+++ gulpfile.js
@@ -13,25 +13,20 @@ function cleanJs() {
             read: false
         })
         .pipe(clean());
-  };
+  }
 
 function js() {
   var jquery = gulp.src('node_modules/jquery/dist/jquery.js');
   var lazyLoad = gulp.src('node_modules/lazyloadxt/dist/jquery.lazyloadxt.js');
   var jQeasing = gulp.src('node_modules/jquery.easing/jquery.easing.js');
   var slickSlider = gulp.src('node_modules/slick-carousel/slick/slick.js');
-  var slickProcess = gulp.src('_includes/components/content/design/slick.js');
-  var slickLarge = gulp.src('_includes/components/containers/card/large.js');
+  var slickProcess = gulp.src('_includes/content/process/slick.js');
+  var slickLarge = gulp.src('_includes/components/card/large.js');
   var lazyScript = gulp.src('_includes/js/lazyLoad.js');
   var pageScroll = gulp.src('_includes/js/pageScroll.js');
-  var navScript = gulp.src('_includes/components/containers/nav/nav.js');
-  var typeFxScript = gulp.src('_includes/components/content/bio/typeFx.js');
-  var prototypeScript = gulp.src('_includes/components/content/prototypes/prototypes.js');
-  var mustache = gulp.src('node_modules/mustache/mustache.js');
-  var behanceProjects = gulp.src('_includes/components/content/design/behanceProjects.js');
-  var instaFeed = request('https://cdnjs.cloudflare.com/ajax/libs/instafeed.js/1.4.1/instafeed.min.js')
-        .pipe(source('instaFeed.js'));
-  var InstaFeedscript = gulp.src('_includes/components/content/art/myInstaFeed.js');
+  var navScript = gulp.src('_includes/components/nav/nav.js');
+  var typeFxScript = gulp.src('_includes/content/bio/typeFx.js');
+  var prototypeScript = gulp.src('_includes/content/prototypes/prototypes.js');
   var gA = gulp.src('_includes/js/g-analytics.js');
 
   return merge(
@@ -46,19 +41,50 @@ function js() {
         slickProcess,
         slickLarge,
         prototypeScript,
-        mustache,
-        behanceProjects,
-        instaFeed,
-        InstaFeedscript,
         gA
     )
     .pipe(buffer())
     .pipe(concat('script.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('assets/'));
-};
+    .pipe(gulp.dest('assets/'));  
+}
+function jsInstagram() {
+  var instaFeed = request('https://cdnjs.cloudflare.com/ajax/libs/instafeed.js/1.4.1/instafeed.min.js')
+        .pipe(source('instaFeed.js'));
+  var InstaFeedscript = gulp.src('_includes/api/instagram/myInstaFeed.js');
 
+  return merge(
+        instaFeed,
+        InstaFeedscript
+    )
+    .pipe(buffer())
+    .pipe(concat('instagram.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('assets/'));  
+}
+
+function jsBehance() {
+  var mustache = gulp.src('node_modules/mustache/mustache.js');
+  var behanceProjects = gulp.src('_includes/api/behance/behanceProjects.js');
+  return merge(
+        mustache,
+        behanceProjects
+    )
+    .pipe(buffer())
+    .pipe(concat('behance.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('assets/'));  
+}
+
+function watch() {
+  gulp.watch("./_includes/**/**/**/*.js", js);
+}
+
+
+gulp.task("watch", watch);
 gulp.task("cleanJs", cleanJs);
 gulp.task("js", js);
+gulp.task("jsInstagram", jsInstagram);
+gulp.task("jsBehance", jsBehance);
 
-gulp.task("default", gulp.series(cleanJs,js));
+gulp.task("default", gulp.series(cleanJs,js,jsInstagram,jsBehance,watch));
