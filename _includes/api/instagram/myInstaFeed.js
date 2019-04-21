4971309c9626c449d61871ebfabeb054939f92a6
diogorusso <hello.diogorusso@gmail.com>
2019-04-20 17:07:46 -0700

    layout

--- _includes/api/instagram/myInstaFeed.js
+++ _includes/api/instagram/myInstaFeed.js
@@ -6,7 +6,7 @@ var feed = new Instafeed({
   accessToken: '2072327.1677ed0.4cc07186a7a047d9b26edfa23dea60a5',
   clientId: 'de2bb2d0384d4f1195c27e61cfd5d594',
   resolution: 'standard_resolution',
-  template: '<div class="col-xs-12 col-sm-6 col-md-4 card-wrap"><article class="card"><a href="{{link}}" target="_blank"><div class="image-frame"><img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxOTIwIiBoZWlnaHQ9IjEwODAiIHZpZXdCb3g9IjAgMCAxOTIwIDEwODAiPgogIDxyZWN0IHdpZHRoPSIxOTIwIiBoZWlnaHQ9IjEwODAiIHg9IjI3MyIgeT0iNDQyIiBmaWxsPSIjRjNGM0YzIiBmaWxsLXJ1bGU9ImV2ZW5vZGQiIHRyYW5zZm9ybT0idHJhbnNsYXRlKC0yNzMgLTQ0MikiLz4KPC9zdmc+Cg=="data-src="{{image}}" alt="{{caption}}"/></div><div class="content"><!--small>likes: {{likes}} | comments: {{comments}}</small--><div class="truncate"><h3>{{caption}}</h3></div></div></a></article></div>',
+  template: '<div class="col-xs-12 col-sm-6 col-md-4" style="margin-bottom:3rem;"><article><a href="{{link}}" target="_blank"><div class="image-frame"><img class="desaturated" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxOTIwIiBoZWlnaHQ9IjEwODAiIHZpZXdCb3g9IjAgMCAxOTIwIDEwODAiPgogIDxyZWN0IHdpZHRoPSIxOTIwIiBoZWlnaHQ9IjEwODAiIHg9IjI3MyIgeT0iNDQyIiBmaWxsPSIjRjNGM0YzIiBmaWxsLXJ1bGU9ImV2ZW5vZGQiIHRyYW5zZm9ybT0idHJhbnNsYXRlKC0yNzMgLTQ0MikiLz4KPC9zdmc+Cg=="data-src="{{image}}" alt="{{caption}}"/></div></div></a></article></div>',
   limit: 6,
   sortBy: 'random',
   link: 'true'
