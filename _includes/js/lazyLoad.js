96f04f13a42ed9b1edecf78f2275739ad87ba2e9
Diogo Russo Ribeiro <diogo.russo.ribeiro@climate.com>
2019-04-24 18:32:35 -0700

    lazy load

--- _includes/js/lazyLoad.js
+++ _includes/js/lazyLoad.js
@@ -1,5 +1,13 @@
+
+
+classLazyHidden = 'lazy-hidden',
+
 $(window).lazyLoadXT();
 // fix for isotope filtering  more: https://isotope.metafizzy.co/events.html#layoutcomplete
-$.lazyLoadXT.updateEvent+=' layoutComplete';
+$.lazyLoadXT.updateEvent+='layoutComplete';
 $.lazyLoadXT.updateEvent+=' afterChange';
-$.lazyLoadXT.onload.addClass = 'img-loaded';
+$.lazyLoadXT.onload.addClass = 'lazy-loaded';
+$.lazyLoadXT.onload.removeClass = classLazyHidden;
+$.lazyLoadXT.onstart.addClass = classLazyHidden;
+$.lazyLoadXT.onerror.removeClass = classLazyHidden;
+
--- _includes/js/pageScroll.js
+++ _includes/js/pageScroll.js
@@ -11,15 +11,15 @@ $(function() {
 var heroBg = $('.hero-post');
     heroTitle = $('.hero-post-box');
     //when scrolltop reaches 35px then opacity of divs is 1 - 35/35 = 0
-    limitTitle = 80;
-    limitBg = 80;  
+    limitTitle = 300;
+    limitBg = 500;  
 
 $(window).on('scroll', function() { 
   
   var st = $(this).scrollTop();
 
   if (st <= limitBg) { 
-    heroBg.css({ 'opacity' : (0.2 + st/limitBg) });
+    heroBg.css({ 'opacity' : (0 + st/limitBg) });
   }
 
   if (st <= limitTitle) { 
