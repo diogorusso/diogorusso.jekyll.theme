62a08f98fc78959f7bc17cff310248115b488134
diogorusso <hello.diogorusso@gmail.com>
2019-05-20 23:21:40 -0700

    lazy load

--- _includes/js/lazyLoad.js
+++ _includes/js/lazyLoad.js
@@ -1,5 +1,5 @@
-
 $(window).lazyLoadXT();
 // fix for isotope filtering  more: https://isotope.metafizzy.co/events.html#layoutcomplete
-$.lazyLoadXT.updateEvent+='layoutComplete';
-$.lazyLoadXT.updateEvent+='afterChange';
+$.lazyLoadXT.updateEvent+=' layoutComplete';
+$.lazyLoadXT.updateEvent+=' afterChange';
+// $.lazyLoadXT.onload.addClass = 'img-loaded';
